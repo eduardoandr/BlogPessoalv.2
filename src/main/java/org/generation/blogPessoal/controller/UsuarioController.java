295782e6423c78191a9bf9ec2ab010c68a148b05
Eduardo <edu3839@hotmail.com>
2021-11-18 13:17:12 -0300

    [Feature Eduardo - Task: Update UsuarioService & UsuarioController | 18.11.21]

--- src/main/java/org/generation/blogPessoal/controller/UsuarioController.java
+++ src/main/java/org/generation/blogPessoal/controller/UsuarioController.java
@@ -9,6 +9,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -36,5 +38,9 @@ public class UsuarioController {
                     throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Usuário já existe");
                 });
     }
+	@GetMapping("/{id}")
+	public ResponseEntity<Usuario> findByIdUsuario(@PathVariable Long id){
+		return usuarioService.findByIdUsuario(id);
+	}
 }
 
